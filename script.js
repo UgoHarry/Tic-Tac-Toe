6aae24c60321de1d9bd850ca7d17b7043202147b
ugo.harry@gmail.com <ugo.harry@gmail.com>
2018-01-25 17:00:36 +1100

    Checkplay and turn functions

--- script.js
+++ script.js
@@ -55,4 +55,43 @@ function turn(elID, player) {
   origBoard[elID] = player;
   //Display their symbol on the grid
   document.getElementById(elID).innerText = player;
+  //check if player has won
+  let gameWon = playcheck(origBoard, player);
+  if (gameWon) {
+    gameOver(gameWon);
+  }
+}
+
+function playcheck(board, plyr) {
+  console.log("Playcheck");
+  //go through the board, and add to new array 
+  // the indexes which have been played by the player
+  let plays = board.reduce( (d, y, i) =>
+    (y === plyr) ? d.concat(i) : d
+  , []);
+  console.log (plays);
+  let gameWon = null;
+  // for every win index and its array item in the winCombos array
+  for(let [index, win] of winCombos.entries() ) {
+    console.log (win);
+    //if every item in the win array is also in the plays array
+    if(win.every(elem =>plays.indexOf(elem) > -1)){
+      //set gameWon to an object 
+      gameWon = {index: index, player: plyr};
+      break;
+    }
+  }
+  return gameWon;
+}
+
+
+function gameOver (gameWon) {
+  for (index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor = 
+      gameWon.player === manPlayer ? "blue" : "red";
+  }
+
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener('click',turnClick, false);
+  }
 }
